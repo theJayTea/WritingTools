32c9d99d7e199df57eb0e8e7b30fb6d4a8afbe98
Jesai Tarun <jesaitarun@gmail.com>
2024-10-23 02:50:40 +0530

    Added work-in-progress macOS code from kurcz

--- macOS/README.md
+++ macOS/README.md
@@ -5,3 +5,5 @@ This folder contains code specific to the macOS port of Writing Tools.
 ## Credits
 The macOS port is being developed by **Jeff Kurcz (kurcz)**.  
 GitHub: [https://github.com/kurcz](https://github.com/kurcz)
+
+Some of the improvements in the v4 release still need to be merged to this macOS codebase.
---
+++ macOS/main.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+from WritingToolApp import WritingToolApp
+
+# Set up logging to console
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def main():
+    """
+    The main entry point of the application.
+    """
+    app = WritingToolApp(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
